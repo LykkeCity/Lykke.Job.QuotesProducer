a5efeea9ccaa68fc2b0e7cd7e4eb828c91c528ed
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-10-31 11:30:44 +0300

    LWDEV-10045: use sdk

---
+++ src/Lykke.Job.QuotesProducer/Settings/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using Lykke.Sdk.Settings;
+
+namespace Lykke.Job.QuotesProducer.Settings
+{
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public class AppSettings : BaseAppSettings
+    {
+        public QuotesProducerSettings QuotesProducerJob { get; set; }
+    }
+}
---
+++ src/Lykke.Job.QuotesProducer/Settings/RabbitConnectionSettings.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Job.QuotesProducer.Settings
+{
+    [UsedImplicitly]
+    public class RabbitConnectionSettings
+    {
+        [AmqpCheck]
+        public string OrderbookSubscription { get; set; }
+        [AmqpCheck]
+        public string QuotesPublication { get; set; }
+    }
+}
